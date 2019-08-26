9cc59cac12979e7a4231a25dbf1bbd74b1fffc51
Jose Quezada <quezada007@yahoo.com>
2019-08-25 19:07:05 -0700

    add react-axe to check for web accessibility issues

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -5,6 +5,14 @@ import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Check for Web Accessibility issues in development mode
+if (process.env.NODE_ENV === 'development') {
+    // eslint-disable-next-line global-require, import/no-extraneous-dependencies
+    const axe = require('react-axe');
+    // Check the console for any accessibility issues
+    axe(React, ReactDOM, 1000);
+}
+
 const App = () => (
     <div>
         <h1>Hello Wold</h1>
