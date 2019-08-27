a37ddb31d914057ee84d23ad3e494193a5cb6a84
Jose Quezada <quezada007@yahoo.com>
2019-08-26 19:29:34 -0700

    update home page

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -13,21 +13,46 @@ if (process.env.NODE_ENV === 'development') {
     axe(React, ReactDOM, 1000);
 }
 
-const App = () => (
-    <div>
-        <h1>Hello Wold</h1>
-        <div>Icons</div>
-        <ul>
-            <li>
-                Home
-                <i className="icon-home" />
-            </li>
-            <li>
-                Checkmark
-                <i className="icon-check" />
-            </li>
-        </ul>
-    </div>
-);
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            count: 0
+        };
+    }
+
+    incrementCount = () => {
+        this.setState((state) => ({ count: state.count + 1 }));
+    }
+
+    render() {
+        const { count } = this.state;
+        return (
+            <div>
+                <h1>Hello Wold</h1>
+                <div>
+                    The count is
+                    {' '}
+                    {count}
+                </div>
+                <button type="button" onClick={this.incrementCount}>Increment Count</button>
+                <div>Icons</div>
+                <ul>
+                    <li>
+                        Home
+                        {' '}
+                        <i className="icon-home" />
+                    </li>
+                    <li>
+                        Checkmark
+                        {' '}
+                        <i className="icon-check" />
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
