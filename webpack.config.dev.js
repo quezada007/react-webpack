192b354c074fd5741529703b2d878f9b42d483f2
Jose Quezada <quezada007@yahoo.com>
2019-08-26 20:54:47 -0700

    update dev server port to 8080

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,7 +5,7 @@ const baseConfig = require('./webpack.config.base');
 module.exports = merge(baseConfig, {
     mode: 'development',
     devServer: {
-        port: 9000,
+        port: 8080,
         open: true,
         hot: true
     },
