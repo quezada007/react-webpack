e44ce9ba3da4a2ca79f6201954939f804acf135c
Jose Quezada <quezada007@yahoo.com>
2021-04-18 20:00:27 -0700

    convert the class component into a function component

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,42 +1,28 @@
-import React, { Suspense, lazy } from 'react';
+import React, { useState, Suspense, lazy } from 'react';
 
 const Message = lazy(() => import('./Message'));
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            count: 0
-        };
-    }
-
-    incrementCount = () => {
-        this.setState((state) => ({ count: state.count + 1 }));
-    }
-
-    render() {
-        const { count } = this.state;
-        return (
-            <div>
-                <h1>Hello World</h1>
-                <p>
-                    The count is
-                    {' '}
-                    {count}
-                </p>
-                <button type="button" onClick={this.incrementCount}>Increment Count</button>
-                <p>When the count reaches 10, a new component will load dynamically.</p>
-                {count >= 10
-                    ? (
-                        <Suspense fallback={<div>Loading...</div>}>
-                            <Message />
-                        </Suspense>
-                    )
-                    : null}
-            </div>
-        );
-    }
-}
+const App = () => {
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>
+                The count is
+                {' '}
+                {count}
+            </p>
+            <button type="button" onClick={() => setCount(count + 1)}>Increment Count</button>
+            <p>When the count reaches 10, a new component will load dynamically.</p>
+            {count >= 10
+                ? (
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Message />
+                    </Suspense>
+                )
+                : null}
+        </div>
+    );
+};
 
 export default App;
